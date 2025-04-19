68b55f3d9b8929ab493e073b26e59f869d28b439
justaveragematt <justaveragematt@gmail.com>
2025-04-19 06:14:51 -0400

    added line drawing tests

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from window import Window
+from window import Line, Point, Window
 
 
 def main():
     win = Window(800, 600)
+    p1 = Point(100, 100)
+    p2 = Point(200, 300)
+    line1 = Line(p1, p2)
+    win.draw_line(line1, "blue")
+
+    p3 = Point(200, 125)
+    p4 = Point(340, 500)
+    line2 = Line(p3, p4)
+    win.draw_line(line2, "red")
+
     win.wait_for_close()
 
 
-main()
+if __name__ == "__main__":
+    main()
