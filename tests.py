1e490e9cf65088c7a13a15edd18f40ceb365db1f
justaveragematt <justaveragematt@gmail.com>
2025-05-03 18:21:11 -0400

    updated all files, maze now builds and self-solves

--- tests.py
+++ tests.py
@@ -30,31 +30,31 @@ class Tests(unittest.TestCase):
             False,
         )
 
-    def test_break_walls(self):
-    # Use a fixed seed for reproducible testing
-    num_cols = 5
-    num_rows = 5
-    m = Maze(0, 0, num_rows, num_cols, 10, 10, seed=42)
-    
-    # Check that all cells are marked as visited
-    for i in range(num_cols):
-        for j in range(num_rows):
-            self.assertTrue(m._cells[i][j].visited)
-    
-    # Check that there is a path from start to end
-    # This is a bit complex to test in a unit test but we could verify
-    # that not all cells have all walls
-    has_broken_walls = False
-    for i in range(num_cols):
-        for j in range(num_rows):
-            cell = m._cells[i][j]
-            if (not cell.has_left_wall or not cell.has_right_wall or 
-                not cell.has_top_wall or not cell.has_bottom_wall):
-                has_broken_walls = True
-                break
-    
-    self.assertTrue(has_broken_walls)
-
+    # def test_break_walls(self):
+    # # Use a fixed seed for reproducible testing
+    # num_cols = 5
+    # num_rows = 5
+    # m = Maze(0, 0, num_rows, num_cols, 10, 10, seed=42)
+    #
+    # # Check that all cells are marked as visited
+    # for i in range(num_cols):
+    #     for j in range(num_rows):
+    #         self.assertTrue(m._cells[i][j].visited)
+    #
+    # # Check that there is a path from start to end
+    # # This is a bit complex to test in a unit test but we could verify
+    # # that not all cells have all walls
+    # has_broken_walls = False
+    # for i in range(num_cols):
+    #     for j in range(num_rows):
+    #         cell = m._cells[i][j]
+    #         if (not cell.has_left_wall or not cell.has_right_wall or
+    #             not cell.has_top_wall or not cell.has_bottom_wall):
+    #             has_broken_walls = True
+    #             break
+    #
+    # self.assertTrue(has_broken_walls)
+    #
     #     # Test 2: Very small maze (1x1)
     #
     # def test_small_maze(self):
@@ -99,6 +99,17 @@ class Tests(unittest.TestCase):
     #         len(m4._cells[0]),
     #         num_rows,
     #     )
+
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in m1._cells:
+            for cell in col:
+                self.assertEqual(
+                    cell.visited,
+                    False,
+                )
 
 
 if __name__ == "__main__":
